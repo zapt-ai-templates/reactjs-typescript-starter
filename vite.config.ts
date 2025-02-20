d2f8928c3a42b46b1927a00dd168d4e893329e16
David Fox <davidmfox123@gmail.com>
2025-02-20 13:01:30 +0000

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "zapt-apps",
+      project: process.env.VITE_PUBLIC_APP_ID,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    })
+  ],
+  build: {
+    target: 'esnext',
+    polyfillDynamicImport: false,
+    sourcemap: true
+  },
   resolve: {
+    conditions: ['development', 'browser'],
     alias: {
       '@': resolve(__dirname, './src')
     }
+  },
+  optimizeDeps: {
+    exclude: ['drizzle-orm']
   }
 });
